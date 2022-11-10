5cd2600db92c8958e1dd1d119bd3e8a9eb839904
neofeelist <neofeelist@gmail.com>
2022-11-10 11:05:17 +0900

    추가됨

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,9 +1,15 @@
 import click
 
 @click.command()
-def cli():
-    lgtm()
-    click.echo('lgtm')
+@click.option('--message', '-m', default='LGTM',
+              show_default=True, help='이미지에 추가할 문자열')
+@click.argument('keyword')
+def cli(keyword, message):
+    """LGTM 이미지 생성 도구"""
+    lgtm(keyword, message)
+    click.echo('lgtm')  # 동작 확인용
 
-def lgtm():
+
+def lgtm(keyword, message):
+    # 여기에 로직을 추가함
     pass
